31595c9fd9cfd5c62afeef3b373fe8c36f458a34
李桂盛 <liguicheng@192.168.0.104>
2020-07-24 21:47:52 +0800

    tabControl点击切换数据

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,9 +6,8 @@
     <home-swiper :banner="banner"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
     <feature-view></feature-view>
-    <tab-control class="tab-control" :titles="['流行','新款','精选']"></tab-control>
-    <goods-list :goods="goods['pop'].list"></goods-list>
-
+    <tab-control class="tab-control" :titles="['流行','新款','精选']" @tabClick="tabClick"></tab-control>
+    <goods-list :goods="showGoods"></goods-list>
   </div>
 </template>
 <script>
@@ -18,7 +17,7 @@ import HomeSwiper from "./childComps/HomeSwiper";
 import RecommendView from "./childComps/RecommendView";
 import FeatureView from "./childComps/FeatureView";
 import TabControl from "components/content/tabControl/TabControl";
-import GoodsList from "components/content/goods/GoodsList"
+import GoodsList from "components/content/goods/GoodsList";
 
 import { getHomeMultidata, getHomeGoods } from "network/home";
 
@@ -30,7 +29,7 @@ export default {
     RecommendView,
     FeatureView,
     TabControl,
-    GoodsList
+    GoodsList,
   },
   data() {
     return {
@@ -39,33 +38,59 @@ export default {
       goods: {
         pop: { page: 0, list: [] },
         new: { page: 0, list: [] },
-        sell: { page: 0, list: [] }
-      }
+        sell: { page: 0, list: [] },
+      },
+      currentType: "pop",
     };
   },
   created() {
     //1.请求多个数据
     this.getHomeMultidata();
     //请求商品数据
-    this.getHomeGoods('pop');
-    this.getHomeGoods('new');
-    this.getHomeGoods('sell');
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
   },
   methods: {
+    /**
+     * 事件监听相关
+     */
+    tabClick(index) {
+      console.log(index);
+      switch (index) {
+        case 0:
+          this.currentType = "pop";
+          break;
+        case 1:
+          this.currentType = "new";
+          break;
+        case 2:
+          this.currentType = "sell";
+          break;
+      }
+    },
+    /**
+     * 网络请求相关
+     */
     getHomeMultidata() {
-      getHomeMultidata().then(res => {
+      getHomeMultidata().then((res) => {
         this.banner = res.data.banner.list;
         this.recommends = res.data.recommend.list;
       });
     },
     getHomeGoods(type) {
       const page = this.goods[type].page + 1;
-      getHomeGoods(type, page).then(res => {
+      getHomeGoods(type, page).then((res) => {
         console.log(res);
         this.goods[type].list.push(...res.data.list);
         this.goods[type].page += 1;
       });
     },
+  },
+  computed: {
+    showGoods() {
+      return this.goods[this.currentType].list
+    }
   }
 };
 </script>
